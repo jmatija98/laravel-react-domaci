7c5ca89d05f434582cf33b931b732982bee1ab92
Zoran Kašćelan <zk20170024@student.fon.bg.ac.rs>
2021-02-13 21:49:04 +0100

    generate pdf: wip

---
+++ laravel/resources/js/convert/post_request.js
@@ -0,0 +1,50 @@
+import {create} from './create_report';
+import $ from 'jquery';
+function convert() {
+
+    const ticket = create('aa', 'aa', 'aaa', 'aaaaa', 'aaaaaaaa');
+
+    var data = {
+        'text': ticket
+    };
+
+    console.log(typeof data);
+    console.log(data)
+
+    var token = btoa('milevJ:4dc40376da1d6e9dc515be148b1a9f21');
+    $.ajax({
+        type: 'POST',
+        url: 'https://api.pdfcrowd.com/convert/',
+        data: data,
+        headers: {
+            'Authorization': 'Basic ' + token
+        },
+        xhrFields: {
+            responseType: 'blob'
+        },
+        success: function (data, xhr) {
+            var blob = new Blob([data], { type: 'application/pdf' });
+            // IE doesn't allow using a blob object directly as link href
+            // instead it is necessary to use msSaveOrOpenBlob
+            if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+                window.navigator.msSaveOrOpenBlob(blob);
+                return;
+            }
+
+            // For other browsers:
+            // Create a link pointing to the ObjectURL containing the blob.
+            const url = window.URL.createObjectURL(blob);
+            var link = document.createElement('a');
+            link.href = url;
+            link.download = 'test_ajax.pdf';
+            link.click();
+            setTimeout(function () {
+                // For Firefox it is necessary to delay revoking the ObjectURL
+                window.URL.revokeObjectURL(url);
+            }, 100);
+        },
+        error: function (xhr) {
+            alert(xhr.responseText);
+        }
+    });
+};
